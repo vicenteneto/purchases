5db0ba194f08df10bec823a90895997614c648bf
Vicente Neto <sneto.vicente@gmail.com>
2017-05-13 20:41:26 -0300

    Creating service to find a specific purchase

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -1,9 +1,22 @@
 module Api::V1
   class PurchasesController < ApplicationController
-    # GET /purchases
+    before_action :set_purchase, only: [:show]
+
+    # GET /v1/purchases
     def index
       @purchases = Purchase.all
       json_response(@purchases)
+    end
+
+    # GET /v1/purchases/:id
+    def show
+      json_response(@purchase)
+    end
+
+    private
+
+    def set_purchase
+      @purchase = Purchase.find(params[:id])
     end
   end
 end
--- spec/requests/purchases_spec.rb
+++ spec/requests/purchases_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe 'Purchases API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  # Test suite for GET /v1/purchases/:id
+  describe 'GET /v1/purchases/:id' do
+    before {get "/v1/purchases/#{purchase_id}"}
+
+    context 'when the record exists' do
+      it 'returns the purchase' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(purchase_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:purchase_id) {100}
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Purchase/)
+      end
+    end
+  end
 end
